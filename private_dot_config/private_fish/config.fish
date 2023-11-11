333dcd5d2f4bddfed6e23aff2a9f3e1fde612f69
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-11-11 21:12:15 +0200

    remove Volta

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,12 +1,5 @@
 # Disable fish greeting
 set -g fish_greeting ""
 
-# Configure Volta
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# Enable experimental pnpm support in Volta
-set -gx VOLTA_FEATURE_PNPM 1
-
 # Initialize Starship prompt
 starship init fish | source
